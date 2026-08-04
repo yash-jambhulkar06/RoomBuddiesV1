8c00973623584dc4692e9109aed0ddd6ca0dda6a
Yash Jambhulkar <jambhulkarn6@gmail.com>
2026-08-04 14:41:36 +0530

    Add WebSocket channel groups for conversations

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -1,14 +1,26 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
         self.conversation_id=self.scope["url_route"]["kwargs"]["conversation_id"]
+        
+        self.room_group_name=f"chat_{self.conversation_id}"
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
+        
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name,
+        )
 
     def receive(self, text_data):
         self.send(text_data=text_data)
